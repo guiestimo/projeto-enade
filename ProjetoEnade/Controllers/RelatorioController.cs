52be45d951bb4670d7c1df7441b694de03f39b42
Guilherme Estimo <guilherme.estimo@hotmail.com>
2021-12-03 16:03:42 -0300

    new features

---
+++ ProjetoEnade/Controllers/RelatorioController.cs
@@ -0,0 +1,78 @@
+﻿using ClosedXML.Excel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ProjetoEnade.Models;
+using ProjetoEnade.Repository;
+using ProjetoEnade.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoEnade.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        private readonly EnadeDbContext _context;
+
+        public RelatorioController(EnadeDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var provas = from prova in _context.Provas.ToList()
+                         join curso in _context.Cursos.ToList() on prova.IdCurso equals curso.Id
+                         select new
+                         {
+                             Id = prova.Id,
+                             DescricaoCompleta = $"Ano: {prova.Ano} - Curso: {curso.Nome} - Edição: {prova.Edicao}"
+                         };
+
+            ViewData["DetalhesProva"] = new SelectList(provas, "Id", "DescricaoCompleta", null);
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Excel(RelatorioModel relatorioModel)
+        {
+            var dadosRelatorio = (from questoes in _context.QuestaoGabarito.ToList()
+                        where
+                        questoes.TipoProva == relatorioModel.TipoProva &&
+                        questoes.DificuldadeQuestao == relatorioModel.DificuldadeQuestao
+                        select new RelatorioModel
+                        {
+                            DescricaoTipoProva = Helpers.GetEnumDescription((Enums.TipoDaProva)questoes.TipoProva),
+                            DescricaoDificuldadeQuestao = Helpers.GetEnumDescription((Enums.DificuldadeDaQuestao)questoes.DificuldadeQuestao),
+                        }).ToList();
+
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Dados");
+                var currentRow = 1;
+                worksheet.Cell(currentRow, 1).Value = "Tipo da Questão";
+                worksheet.Cell(currentRow, 2).Value = "Dificuldade da Questão";
+                foreach (var item in dadosRelatorio)
+                {
+                    currentRow++;
+                    worksheet.Cell(currentRow, 1).Value = item.DescricaoTipoProva;
+                    worksheet.Cell(currentRow, 2).Value = item.DescricaoDificuldadeQuestao;
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+
+                    return File(
+                        content,
+                        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                        "users.xlsx");
+                }
+            }
+        }
+    }
+}
---
+++ ProjetoEnade/Models/RelatorioModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoEnade.Models
+{
+    public class RelatorioModel
+    {
+        [Display(Name = "Data Início da Prova")]
+        public DateTime DataProvaInicio { get; set; }
+
+        [Display(Name = "Data Fim da Prova")]
+        public DateTime DataProvaFim { get; set; }
+
+        [Display(Name = "Tipo da Questão")]
+        public int TipoProva { get; set; }
+        public string DescricaoTipoProva { get; set; }
+
+        [Display(Name = "Dificuldade da Questão")]
+        public int DificuldadeQuestao { get; set; }
+        public string DescricaoDificuldadeQuestao { get; set; }
+
+        [Display(Name = "Prova")]
+        public int IdProva { get; set; }
+
+        [Display(Name = "Resposta Correta")]
+        public int RespostaCorreta { get; set; }
+    }
+}
